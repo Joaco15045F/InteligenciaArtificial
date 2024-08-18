dc71bc6c95541565535b46ccfe2a300472ee9c9f
Joaco15045F <joaco.ve15@gmail.com>
2024-08-18 17:20:11 -0400

    Laboratorio 1

---
+++ Practicos/Practico1/practicocodsolo.py
@@ -0,0 +1,20 @@
+alturas=[150,160,180,197,155,177,182,154,178,200]
+pesos=[55,58,82,96,50,70,86,50,76,99]
+
+#print(alturas)
+#print(pesos)
+ErrorMejor = 0
+for A in range(-100, 101):
+    error = 0
+    for B in range(-100, 101):
+        for i in range(len(alturas)):
+            y = A * alturas[i] + B
+        error = 
+        
+        
+        
+        
+#Buscar en Kaggle de regresion lineal simple (si es multiple, tomar solo 2 columnas)
+#eso luego aplicaremos al cuadernillo que veremos ahora
+#es trabajo de 2 personas
+#
